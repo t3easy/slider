2e6b368983ba66e396639944bca845a98b091eea
Jan Kiesewetter <jan@t3easy.de>
2023-09-17 20:33:52 +0200

    [TASK] Add composer.json and ext_emconf.php

---
+++ ext_emconf.php
@@ -0,0 +1,21 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Slider',
+    'description' => 'Slider',
+    'category' => 'fe',
+    'author' => 'Jan Kiesewetter',
+    'author_email' => 'jan@t3easy.de',
+    'author_company' => 't3easy',
+    'state' => 'stable',
+    'version' => '0.1.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '9.5.0-12.4.99',
+            'fluid' => '9.5.0-12.4.99',
+            'frontend' => '9.5.0-12.4.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
